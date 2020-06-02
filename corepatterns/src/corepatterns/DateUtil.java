cc29e7269e935d950d0c8bee3d8e40f7f9f64a22
Ankit Tyagi <ankittyaagi@gmail.com>
2020-06-03 02:42:44 +0530

    initial commit

---
+++ corepatterns/src/corepatterns/DateUtil.java
@@ -0,0 +1,20 @@
+package corepatterns;
+
+public class DateUtil {
+
+	private static DateUtil instance;
+	
+	private DateUtil()
+	{
+		
+	}
+	
+	public static DateUtil getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new DateUtil();
+		}
+		return instance;
+	}
+}
