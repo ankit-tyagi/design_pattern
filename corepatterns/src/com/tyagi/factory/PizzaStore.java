425bee72ba5c9e505cd50543accf568954f77936
Ankit Tyagi <ankittyaagi@gmail.com>
2020-06-05 02:56:49 +0530

    factory pattern

--- corepatterns/src/com/tyagi/factory/PizzaStore.java
+++ corepatterns/src/com/tyagi/factory/PizzaStore.java
@@ -3,15 +3,7 @@ package com.tyagi.factory;
 public class PizzaStore {
 	
 	public Pizza orderPizza(String type) {
-		Pizza p = null;
-		
-		if(type.equals("cheese")) {
-			p = new CheesePizza();
-		}else if(type.equals("chicken")) {
-			p = new ChickenPizza();
-		}else if(type.equals("veggie")) {
-			p = new VeggiePizza();
-		}
+		Pizza p = PizzaFactory.createPizza(type);
 		
 		p.prepare();
 		p.bake();
